d1899cf4dd07d8d861f029c2219b6a0f984217c9
Josef Hedblom <hedblomjosef@gmail.com>
2024-10-16 19:05:36 +0200

    Create FileManager class skeleton.

--- src/BestGymEver/FileManager.java
+++ src/BestGymEver/FileManager.java
@@ -1,4 +1,33 @@
 package BestGymEver;
 
+import java.io.*;
+
 public class FileManager {
+    private String fileNamePath;
+    private String fileLogPath;
+
+    public FileManager(String fileNamePath, String fileLogPath) {
+        this.fileNamePath = fileNamePath;
+        this.fileLogPath = fileLogPath;
+    }
+
+    public void readFromFile(){
+        try(BufferedReader fileReader = new BufferedReader(new FileReader(this.fileNamePath))){
+            String line;
+            while((line = fileReader.readLine()) != null){
+                System.out.println(line);
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void writeToFile(String text){
+        try(BufferedWriter fileWriter = new BufferedWriter(new FileWriter(this.fileLogPath))){
+            fileWriter.write(String.join("\n", text));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
